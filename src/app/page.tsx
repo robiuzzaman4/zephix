e1c0257c21f2cf947f384eb440a6e1adf5800d59
ruhan <robiuzzaman4@gmail.com>
2025-04-25 23:47:06 +0600

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-medium text-rose-500">HomePage</h1>
+    </div>
+  )
+}
+
+export default HomePage
