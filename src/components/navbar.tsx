1609d155753a0a687a4bb49f0d3fb598a8f8ee6f
ruhan <robiuzzaman4@gmail.com>
2025-05-04 21:45:49 +0600

    feat: add mobile navigation sheet and update navbar for responsive design
    - Implemented a mobile-friendly navigation sheet using Radix UI.
    - Updated Navbar component to include mobile navigation functionality.
    - Added new dependencies: @radix-ui/react-dialog, class-variance-authority, lucide-react, tw-animate-css.
    - Updated package.json and package-lock.json with new dependencies.
    - Imported tw-animate-css in globals.css for animations.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,6 +5,14 @@ import Container from "@/components/shared/container";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 
 const navlinks = [
   {
@@ -33,19 +41,49 @@ const Navbar = () => {
         <Link href="/" className="text-3xl font-medium">
           Zephix
         </Link>
-        <div className="flex items-center justify-center gap-6">
+        <div className="hidden lg:flex items-center justify-center gap-6">
           {navlinks.map((link) => (
             <Link
               href={link.href}
               key={link.label}
               className={cn("text-base font-medium", {
-                "text-primary bg-secondary px-2 py-0.5 rounded-md": pathname === link.href,
+                "text-primary bg-secondary px-2 py-0.5 rounded-md":
+                  pathname === link.href,
               })}
             >
               {link.label}
             </Link>
           ))}
         </div>
+
+        {/* mobile navbar */}
+        <Sheet>
+          <SheetTrigger className="lg:hidden">Open</SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader className="sr-only">
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+
+            <div className="flex flex-col gap-2 mt-6">
+              {navlinks.map((link) => (
+                <Link
+                  href={link.href}
+                  key={link.label}
+                  className={cn("text-base font-medium w-full px-4 py-1", {
+                    "text-primary bg-secondary rounded-md":
+                      pathname === link.href,
+                  })}
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
       </Container>
     </nav>
   );
